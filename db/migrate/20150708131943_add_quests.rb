77057dfd34de8fb817f0219ef198735917274411
syzygy333 <brianjonashogue@gmail.com>
2015-07-08 09:24:04 -0400

    add quests table

---
+++ db/migrate/20150708131943_add_quests.rb
@@ -0,0 +1,11 @@
+class AddQuests < ActiveRecord::Migration
+  def change
+    create_table :quests do |t|
+      t.string :title, null: false
+      t.text :description
+      t.date :start_date, null: false
+      t.date :end_date, null: false
+      t.integer :user_id, null: false
+    end
+  end
+end
