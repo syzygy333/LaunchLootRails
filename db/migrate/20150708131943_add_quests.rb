21e7c222fb8b89e645738b2c4a01425e26f4c9f8
syzygy333 <brianjonashogue@gmail.com>
2015-07-09 08:54:23 -0400

    add migration add quest model

--- db/migrate/20150708131943_add_quests.rb
+++ db/migrate/20150708131943_add_quests.rb
@@ -6,6 +6,8 @@ class AddQuests < ActiveRecord::Migration
       t.date :start_date, null: false
       t.date :end_date, null: false
       t.integer :user_id, null: false
+
+      t.timestamps
     end
   end
 end
