4f7b03928398bdd29200a0ec9eb0d6a7b30e4a82
syzygy333 <brianjonashogue@gmail.com>
2015-07-09 09:07:48 -0400

    add quest controller, add quest index, update routes

---
+++ app/controllers/quests_controller.rb
@@ -0,0 +1,26 @@
+class QuestsController < ApplicationController
+  def index
+    @quests = Quest.all
+  end
+
+  def new
+    @quest = Quest.new
+  end
+
+  def create
+    @quest = Quest.new(quest_params)
+    if @quest.save
+      flash[:success] = "It shall be so."
+      redirect_to quests_path
+    else
+      flash[:alert] = @spot.errors.full_messages.join(".  ")
+      render :new
+    end
+  end
+
+  private
+
+  def quest_params
+    params.require(:quest).permit(:title, :description, :start_date, :end_date, :user_id)
+  end
+end
