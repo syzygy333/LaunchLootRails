eed1e4fa0d345f4705b509b67330e9b6add627ad
syzygy333 <brianjonashogue@gmail.com>
2015-07-15 11:39:17 -0400

    hound

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,8 @@
 module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
-    if css_class = column == sort_column
-      "current #{sort_direction}"
-    else
-      nil
-    end
-    if direction = column == sort_column && sort_direction ==
-      "asc"
-      "desc"
-    else
-      "asc"
-    end
-    link_to title, { sort: column, direction: direction },
-      class: css_class
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, { sort: column, direction: direction }, class: css_class
   end
 end
