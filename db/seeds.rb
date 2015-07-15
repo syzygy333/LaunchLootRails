37f3179dd9ad8da323f7e2d089873d81322925c7
syzygy333 <brianjonashogue@gmail.com>
2015-07-14 22:59:49 -0400

    hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,19 @@ descriptor2 = CSV.read("app/assets/csv/descriptor2.csv")
 250.times do
   Item.create(name: "#{descriptor1[0].sample.titleize}
     #{ordinary[0].sample.titleize} of #{descriptor2[0].sample.titleize}",
-    rarity: "ordinary", value: 100 )
+    rarity: "ordinary", value: 100)
 end
 
 150.times do
   Item.create(name: "#{descriptor1[0].sample.titleize}
     #{heirloom[0].sample.titleize} of #{descriptor2[0].sample.titleize}",
-    rarity: "heirloom", value: 250 )
+    rarity: "heirloom", value: 250)
 end
 
 75.times do
   Item.create(name: "#{descriptor1[0].sample.titleize}
     #{antiquity[0].sample.titleize} of
-    #{descriptor2[0].sample.titleize}", rarity: "antiquity", value: 500 )
+    #{descriptor2[0].sample.titleize}", rarity: "antiquity", value: 500)
 end
 
 # 25.times do
