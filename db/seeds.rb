ac62990d2e929fc21a1e3ea721844b530e616285
syzygy333 <brianjonashogue@gmail.com>
2015-07-14 22:58:15 -0400

    hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,21 @@ antiquity = CSV.read("app/assets/csv/antiquity.csv")
 descriptor2 = CSV.read("app/assets/csv/descriptor2.csv")
 
 250.times do
-  Item.create(name: "#{descriptor1[0].sample.titleize} #{ordinary[0].sample.titleize} of #{descriptor2[0].sample.titleize}", rarity: "ordinary", value: 100 )
+  Item.create(name: "#{descriptor1[0].sample.titleize}
+    #{ordinary[0].sample.titleize} of #{descriptor2[0].sample.titleize}",
+    rarity: "ordinary", value: 100 )
 end
 
 150.times do
-  Item.create(name: "#{descriptor1[0].sample.titleize} #{heirloom[0].sample.titleize} of #{descriptor2[0].sample.titleize}", rarity: "heirloom", value: 250 )
+  Item.create(name: "#{descriptor1[0].sample.titleize}
+    #{heirloom[0].sample.titleize} of #{descriptor2[0].sample.titleize}",
+    rarity: "heirloom", value: 250 )
 end
 
 75.times do
-  Item.create(name: "#{descriptor1[0].sample.titleize} #{antiquity[0].sample.titleize} of #{descriptor2[0].sample.titleize}", rarity: "antiquity", value: 500 )
+  Item.create(name: "#{descriptor1[0].sample.titleize}
+    #{antiquity[0].sample.titleize} of
+    #{descriptor2[0].sample.titleize}", rarity: "antiquity", value: 500 )
 end
 
 # 25.times do
