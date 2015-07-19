cd3750416626dd7e389dda006e3c1005bcb62437
syzygy333 <brianjonashogue@gmail.com>
2015-07-19 16:49:09 -0400

    change secret key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714161200) do
+ActiveRecord::Schema.define(version: 20150719202733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20150714161200) do
   end
 
   create_table "finds", force: :cascade do |t|
-    t.integer "quest_id", null: false
-    t.integer "item_id",  null: false
-    t.integer "user_id",  null: false
+    t.integer  "quest_id",   null: false
+    t.integer  "item_id",    null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "items", force: :cascade do |t|
@@ -32,6 +34,16 @@ ActiveRecord::Schema.define(version: 20150714161200) do
     t.string  "rarity", null: false
     t.integer "value",  null: false
   end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "quests", force: :cascade do |t|
     t.string   "title",       null: false
