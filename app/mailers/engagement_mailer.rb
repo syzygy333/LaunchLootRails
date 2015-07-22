3076536bf289077544abbaf09f3c5e25d0e4ac48
syzygy333 <brianjonashogue@gmail.com>
2015-07-22 14:23:27 -0400

    change URL to localhost for demonstration

--- app/mailers/engagement_mailer.rb
+++ app/mailers/engagement_mailer.rb
@@ -1,7 +1,7 @@
 class EngagementMailer < ApplicationMailer
   def new_engagement(engagement, number)
     @engagement = engagement
-    @url = "http://launchloot.herokuapp.com/finds/#{number}"
+    @url = "http://localhost:3000/finds/#{number}"
     mail(
       to: engagement.user.email,
       subject: "New LaunchLoot awaits!"
