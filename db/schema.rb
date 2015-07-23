2bce45cf003ec92c3c317f4c1b389947c55eb6af
syzygy333 <brianjonashogue@gmail.com>
2015-07-23 14:59:07 -0400

    add equipped? to items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721165218) do
+ActiveRecord::Schema.define(version: 20150723182210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,11 @@ ActiveRecord::Schema.define(version: 20150721165218) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string  "name",   null: false
-    t.string  "rarity", null: false
-    t.integer "value",  null: false
+    t.string  "name",                      null: false
+    t.string  "rarity",                    null: false
+    t.integer "value",                     null: false
+    t.boolean "equipped?", default: false, null: false
+    t.integer "priority"
   end
 
   create_table "quests", force: :cascade do |t|
